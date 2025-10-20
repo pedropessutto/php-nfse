b84c07e7cdbcae59433cf147660f1c0e878837a9
pedropessutto <pedrospessutto@gmail.com>
2025-10-20 18:24:54 -0300

    Emissão NFS-e Nova Prata

---
+++ src/Counties/M4313300/Response.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace NFePHP\NFSe\Counties\M4313300;
+
+/**
+ * Classe para extração dos dados retornados pelos webservices da
+ * Cidade de Nova Prata RS conforme o modelo TECNOS
+ *
+ * @category  NFePHP
+ * @package   NFePHP\NFSe\Counties\M4308201\Response
+ * @copyright NFePHP Copyright (c) 2016
+ * @license   http://www.gnu.org/licenses/lgpl.txt LGPLv3+
+ * @license   https://opensource.org/licenses/MIT MIT
+ * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
+ * @author    José Alcides Benetti de Souza
+ * @link      http://github.com/nfephp-org/sped-nfse for the canonical source repository
+ */
+
+use NFePHP\NFSe\Models\Tecnos\Response as ResponseModel;
+
+class Response extends ResponseModel
+{
+}
---
+++ src/Counties/M4313300/Rps.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace NFePHP\NFSe\Counties\M4313300;
+
+/**
+ * Classe a construção do xml da NFSe para a
+ * Cidade de Nova Prata RS
+ * conforme o modelo ISSNET
+ *
+ * @category  NFePHP
+ * @package   NFePHP\NFSe\Counties\M4308201\Rps
+ * @copyright NFePHP Copyright (c) 2016
+ * @license   http://www.gnu.org/licenses/lgpl.txt LGPLv3+
+ * @license   https://opensource.org/licenses/MIT MIT
+ * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
+ * @author    José Alcides Benetti de Souza
+ * @link      http://github.com/nfephp-org/sped-nfse for the canonical source repository
+ */
+
+use NFePHP\NFSe\Models\Tecnos\Rps as RpsModel;
+
+class Rps extends RpsModel
+{
+
+}
